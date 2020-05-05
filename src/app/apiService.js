19af5e25456f6e3ab7618b8f245050d0fd6ff994
Júnior Trajano Rodrigues <junior.rodrigues@softplan.com.br>
2020-05-04 23:19:57 -0300

    teste

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'https://jrfinancas-api.herokuapp.com', 
+    baseURL: 'https://jrfinancas-api.herokuapp.com'
     //'http://localhost:8080',
-    headers:{'x-apikey': '59a7ad19f5a9fa0808f11931',
-             'Access-Control-Allow-Origin' : '*',
-             'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-    }
+    
+    //headers:{'x-apikey': '59a7ad19f5a9fa0808f11931',
+    //         'Access-Control-Allow-Origin' : '*',
+    //         'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    //}
 });
 
 class ApiService{
